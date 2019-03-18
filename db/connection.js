4c01d649e372d6fceafa2fb3c4fc0f1986cb3610
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-03-18 17:27:04 -0400

    modified connection.js to be deploy ready. NVC

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/nba-roster");
 mongoose.Promise = Promise;
+
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.DB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/nba-roster");
+}
+
 module.exports = mongoose;
