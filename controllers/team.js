726e15180d45e217495b31a707eb1a295ec990a7
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-03-19 16:26:52 -0400

    modified controller/team method update to update team by id instead of name

--- controllers/team.js
+++ controllers/team.js
@@ -1,26 +1,26 @@
 const { Team } = require("../models/index");
 
 module.exports = {
-    index: function (req, res) {
-        Team.find({}).then(teams => {
-            res.json(teams)
-        });
-    },
-    create: (req, res) => {
-        Team.create(req.body).then(team => res.json(team));
-        console.log(req.body);
-    },
-    update: (req, res) => {
-        Team.findOne({ name: req.params.name }).then(team => {
-            team.fullName = req.body.fullName;
-            team.save((err, team) => {
-                res.json(team);
-            });
-        });
-    },
-    delete: function (req, res) {
-        Team.findByIdAndRemove({ _id: req.params.id }).then(team => {
-            res.json(team);
-        });
-    }
+  index: function(req, res) {
+    Team.find({}).then(teams => {
+      res.json(teams);
+    });
+  },
+  create: (req, res) => {
+    Team.create(req.body).then(team => res.json(team));
+    console.log(req.body);
+  },
+  update: (req, res) => {
+    Team.findOne({ _id: req.params.id }).then(team => {
+      team.fullName = req.body.fullName;
+      team.save((err, team) => {
+        res.json(team);
+      });
+    });
+  },
+  delete: function(req, res) {
+    Team.findByIdAndRemove({ _id: req.params.id }).then(team => {
+      res.json(team);
+    });
+  }
 };
