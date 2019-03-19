0cfa3350454187ddc9b5a8ce73e6e0a1ded20b62
Ryan White <mrwhiterk@gmail.com>
2019-03-19 14:48:17 -0400

    changed Players.js to player.js

--- db/playersSeed.js
+++ db/playersSeed.js
@@ -1,47 +1,46 @@
-const { Player } = require("../models/index");
+const { Player } = require('../models/index');
 
 Player.remove({}).then(_ => {
   Player.insertMany([
     {
-      firstName: "Alex",
-      lastName: "Abrines",
-      personId: "203518",
-      pos: "G-F",
-      heightFeet: "6",
-      heightInches: "6",
-      dateOfBirthUTC: "1993-08-01",
-      nbaDebutYear: "2016",
-      yearsPro: "0",
-      collegeName: "",
-      country: "Spain"
+      firstName: 'Alex',
+      lastName: 'Abrines',
+      personId: '203518',
+      pos: 'G-F',
+      heightFeet: '6',
+      heightInches: '6',
+      dateOfBirthUTC: '1993-08-01',
+      nbaDebutYear: '2016',
+      yearsPro: '0',
+      collegeName: '',
+      country: 'Spain',
     },
     {
-      firstName: "Quincy",
-      lastName: "Acy",
-      personId: "203112",
-      pos: "F",
-      heightFeet: "6",
-      heightInches: "7",
-      dateOfBirthUTC: "1990-10-06",
-      nbaDebutYear: "2012",
-      yearsPro: "4",
-      collegeName: "Baylor",
-      country: "USA"
+      firstName: 'Quincy',
+      lastName: 'Acy',
+      personId: '203112',
+      pos: 'F',
+      heightFeet: '6',
+      heightInches: '7',
+      dateOfBirthUTC: '1990-10-06',
+      nbaDebutYear: '2012',
+      yearsPro: '4',
+      collegeName: 'Baylor',
+      country: 'USA',
     },
-
     {
-      firstName: "Steven",
-      lastName: "Adams",
-      personId: "203500",
-      pos: "C",
-      heightFeet: "7",
-      heightInches: "0",
-      dateOfBirthUTC: "11993-07-20",
-      nbaDebutYear: "2013",
-      yearsPro: "3",
-      collegeName: "Pittsburgh",
-      country: "New Zealand"
-    }
+      firstName: 'Steven',
+      lastName: 'Adams',
+      personId: '203500',
+      pos: 'C',
+      heightFeet: '7',
+      heightInches: '0',
+      dateOfBirthUTC: '11993-07-20',
+      nbaDebutYear: '2013',
+      yearsPro: '3',
+      collegeName: 'Pittsburgh',
+      country: 'New Zealand',
+    },
   ]).then(player => {
     console.log(player);
     process.exit();
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('../db/connection');
 
 module.exports = {
-  Player: mongoose.model('Player', require('./players')),
+  Player: mongoose.model('Player', require('./player')),
 };
