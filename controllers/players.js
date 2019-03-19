24192e030ccaf7bd1c62702b51871352432f6ba4
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-03-18 21:32:53 -0400

    Added create method to the controllers/players and  rourtes/players.js NVC

--- controllers/players.js
+++ controllers/players.js
@@ -6,10 +6,15 @@ module.exports = {
       res.json(players);
     });
   },
+
+  create: (req, res) => {
+    Players.create(req.body).then(player => res.json(player));
+    console.log(req.body);
+  },
+
   delete: (req, res) => {
     Players.findByIdAndRemove({ _id: req.params.id }).then(player => {
       res.json(player);
-      console.log(player);
     });
   }
 };
--- routes/players.js
+++ routes/players.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/players");
 
 router.get("/", controller.index);
+router.post("/", controller.create);
 router.delete("/:id", controller.delete);
 
 module.exports = router;
