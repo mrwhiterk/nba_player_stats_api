211990240d35657649edbe1ed2fea8bfc821020f
Ryan White <mrwhiterk@gmail.com>
2019-03-19 14:19:53 -0400

    test working heroku

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const PlayerRoutes = require('./players');
 
-router.use('/players', require('./players'));
+router.use('/players', PlayerRoutes);
 
 module.exports = router;
