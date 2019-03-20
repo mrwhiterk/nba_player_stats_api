1a57fb54fb0f72b1ecdf652730f0a0d3a8cc71e9
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-03-20 10:09:42 -0400

    merged with upstream.  NVC

--- controllers/team.js
+++ controllers/team.js
@@ -1,4 +1,4 @@
-const { Team } = require('../models/index');
+const { Team } = require("../models/index");
 
 module.exports = {
   index: function(req, res) {
@@ -6,29 +6,16 @@ module.exports = {
       res.json(teams);
     });
   },
-<<<<<<< HEAD
-
-  show: (req, res) => {
-    Team.find({ _id: req.params.id }).then(team => {
-=======
   show: function(req, res) {
     Team.findOne({ _id: req.params.id }).then(team => {
->>>>>>> upstream/master
       res.json(team);
     });
   },
   create: (req, res) => {
     Team.create(req.body).then(team => res.json(team));
-<<<<<<< HEAD
   },
   update: (req, res) => {
     Team.findOne({ _id: req.params.id }).then(team => {
-=======
-    console.log(req.body);
-  },
-  update: (req, res) => {
-    Team.findOne({ name: req.params.name }).then(team => {
->>>>>>> upstream/master
       team.fullName = req.body.fullName;
       team.save((err, team) => {
         res.json(team);
@@ -40,7 +27,6 @@ module.exports = {
       res.json(team);
     });
   },
-<<<<<<< HEAD
   removeTeamPlayer: (req, res) => {
     Team.findOne({ _id: req.params.teamId }).then(team => {
       team.teamRoster = team.teamRoster.filter(
@@ -52,6 +38,4 @@ module.exports = {
       });
     });
   }
-=======
->>>>>>> upstream/master
 };
