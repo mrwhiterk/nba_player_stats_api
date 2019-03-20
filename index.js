f57a5dbd81f70e2f73fe1218f26d03e4668fe0d0
Joshua Vance <skinsfandc0612@icloud.com>
2019-03-20 09:54:34 -0400

    starting to build out authenitication model

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const parser = require("body-parser");
 const cors = require("cors");
+const passport = require("passport")
+const passport = require('./config/passport')()
+
 
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
+app.use(passport.initialize())
 app.use(parser.json());
 app.use(cors());
+
 
 app.use(require("./routes/index.js"));
 
