4e23ed2874c455dd9fd6d9d4f15fcc4359ee483a
Joshua Vance <skinsfandc0612@icloud.com>
2019-03-21 10:39:08 -0400

    added the login feature on the backend jfv

--- controllers/user.js
+++ controllers/user.js
@@ -1,41 +1,62 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jwt-simple')
-const passport = require('../config/passport')
-const config = require('../config/config')
-const { User } = require('../models/index')
+const express = require('express');
+const router = express.Router();
+const jwt = require('jwt-simple');
+const passport = require('../config/passport');
+const config = require('../config/config');
+const { User } = require('../models/index');
 
 router.post('/signup', (req, res) => {
-    if (req.body.email && req.body.password) {
-        let newUser = {
-            email: req.body.email,
-            password: req.body.password
+  if (req.body.email && req.body.password) {
+    let newUser = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    User.findOne({ email: req.body.email }).then(user => {
+      if (!user) {
+        User.create(newUser).then(user => {
+          if (user) {
+            var payload = {
+              id: newUser.id,
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+              token: token,
+            });
+          } else {
+            res.sendStatus(401);
+          }
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.post('/login', (req, res) => {
+  if (req.body.email && req.body.password) {
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        if (user.password === req.body.password) {
+          var payload = {
+            id: user.id,
+          };
+          var token = jwt.encode(payload, config.jwtSecret);
+          res.json({
+            token: token,
+          });
+        } else {
+          res.sendStatus(401);
         }
-        User.findOne({ email: req.body.email })
-            .then((user) => {
-                if (!user) {
-                    User.create(newUser)
-                        .then(user => {
-                            if (user) {
-                                var payload = {
-                                    id: newUser.id
-                                }
-                                var token = jwt.encode(payload, config.jwtSecret)
-                                res.json({
-                                    token: token
-                                })
-                            } else {
-                                res.sendStatus(401)
-                            }
-                        })
-                } else {
-                    res.sendStatus(401)
-                }
-            })
-    } else {
-        res.sendStatus(401)
-    }
-})
+      } else {
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
